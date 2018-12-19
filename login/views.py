7c338018ce5c6477ba0b759f5f1e46c1c96c3226
Sam <Sam@example.com>
2018-12-19 16:22:57 +0800

    modified login views

--- login/views.py
+++ login/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 
+from . import models
+
 # Create your views here.
 
 def index(request):
@@ -10,7 +12,29 @@ def index(request):
     return render(request, 'login/index.html')
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        #print(username, password)
+        message = '所有字段都必须填写'
+        if username and password:
+            username = username.strip()
+            #password = password.strip()
+            #print(username, password)
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    #print('ok')
+                    return redirect('/index/')
+                else:
+                    #print('no,the password: "%s"' % password)
+                    message = '密码错误'
+            except:
+                message = '用户名不存在'
+        context = {
+            'message': message,
+        }
+        return render(request, 'login/login.html', context)
     return render(request, 'login/login.html')
 
 def register(request):
