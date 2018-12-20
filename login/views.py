92122b6cb74a0b0c089543d0299705b8f1d97748
Sam <Sam@example.com>
2018-12-20 16:36:07 +0800

    create register views

--- login/views.py
+++ login/views.py
@@ -51,8 +51,43 @@ def login(request):
     return render(request, 'login/login.html', locals())
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+    if request.method == 'POST':
+        register_form = forms.RegisterForm(request.POST)
+        #message = '所有字段都必须填写正确'
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            if password1 == password2:
+                same_name_user = models.User.objects.filter(name=username)
+                # 用户名唯一
+                if same_name_user:
+                    message = '用户名已存在!'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = '该邮箱已经注册!'
+                    return render(request, 'login/register.html', locals())
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return redirect('/login/')
+            else:
+                message = '二次密码不一致'
+                return render(request, 'login/register.html', locals())
+        else:
+            message = '所有字段都必须填写正确'
+            return render(request, 'login/register.html', locals())
+
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 def logout(request):
     if not request.session.get('is_login', None):
